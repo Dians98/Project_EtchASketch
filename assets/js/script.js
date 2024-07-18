f8515532e125f22f58cea84ebdac76d2d42bbf30
Diano <Diano.faniry@gmail.com>
2024-07-18 15:15:21 +0400

    Etch A Sketch v1 done!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,44 @@
-alert("Hello");
+const gridBtn = document.querySelector("#gridBtn");
+let gridNumber;
+
+gridBtn.addEventListener("click", () => {
+    gridNumber = parseInt(document.querySelector("#gridNumberInput").value);
+    initializeGrid(gridNumber);
+    const closeModalBtn = document.querySelector("#closeModal");
+    closeModalBtn.click();
+});
+
+const initializeGrid = function (number) {
+    reinitializeGrid();
+    if (number != 0) {
+        const grid = document.querySelector("#grid");
+
+        for (let i = 0; i < number; i++) {
+            const newDivLine = document.createElement('div');
+            newDivLine.id = 'gridLine';
+            newDivLine.className = 'gridLine' + i;
+            newDivLine.style.cssText = "display: flex;";
+            grid.appendChild(newDivLine);
+            for (let j = 0; j < number; j++) {
+                const newDivColumn = document.createElement('div');
+                newDivColumn.id = 'gridLine' + i + "Column" + j;
+                newDivColumn.className = 'gridLine' + i + "Column" + j;
+                newDivColumn.style.cssText = "height : 50px; width : 50px; border : 1px solid black";
+                newDivColumn.addEventListener('mouseover', function () {
+                    newDivColumn.style.backgroundColor = 'yellow'; // Changez la couleur de fond ou ajoutez d'autres styles
+                });
+                newDivLine.appendChild(newDivColumn);
+            }
+
+        }
+    }
+};
+
+const reinitializeGrid = function () {
+    const gridLines = document.querySelectorAll("#gridLine");
+    if (gridLines) {
+        gridLines.forEach(gridLine => {
+            gridLine.remove();
+        });
+    }
+}
